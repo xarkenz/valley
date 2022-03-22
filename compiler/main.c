852ccf0bd38773468841acffd6fbe769923d778d
xarkenz <seanedwardsclarke@gmail.com>
2022-03-21 22:35:29 -0500

    Tried to make it work (it didn't)

--- compiler/main.c
+++ compiler/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+#include "include/valley.h"
+
 int main() {
-    printf("Hello, World!\n");
+    VLParser parser = {fopen("test.vl", "r"), -1, {}};
+    vlNextToken(&parser);
+    while (parser.token.kind != VL_TOKEN_EOF) {
+        printf("%s\n", parser.token.stringValue.first);
+        vlNextToken(&parser);
+    }
     return 0;
 }
